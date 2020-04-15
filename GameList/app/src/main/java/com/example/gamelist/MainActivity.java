2a1b2a90c0df0c83627ff7a3325aad81fffeb5b5
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-15 19:23:10 +0530

    game-list 90% completed

--- GameList/app/src/main/java/com/example/gamelist/MainActivity.java
+++ GameList/app/src/main/java/com/example/gamelist/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.gamelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_main);
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.layout, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
     }
 }
