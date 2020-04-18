95ae95e05947f07072403e8eafd492cd697a2761
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-18 09:34:51 +0530

    intent try

--- Intent3/app/src/main/java/com/example/intent3/SecondActivity.java
+++ Intent3/app/src/main/java/com/example/intent3/SecondActivity.java
@@ -1,14 +1,17 @@
 package com.example.intent3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
+        setContentView(R.layout.activity_second);
+
+        Toast.makeText(getApplicationContext(), "We are moved to second Activity",Toast.LENGTH_LONG).show();
     }
 }
