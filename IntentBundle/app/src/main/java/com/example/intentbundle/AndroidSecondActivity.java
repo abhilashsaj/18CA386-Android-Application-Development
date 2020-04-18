7abd2057eaee84de4c44aafd88d6720efe384891
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-18 09:50:00 +0530

    updates

---
+++ IntentBundle/app/src/main/java/com/example/intentbundle/AndroidSecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentbundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AndroidSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_second);
+    }
+}
