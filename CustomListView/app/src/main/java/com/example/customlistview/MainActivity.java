0c5d18b7659f8eb35eaae1d0b1f8aa55e2ae56b0
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-18 11:26:58 +0530

    updates

--- CustomListView/app/src/main/java/com/example/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/customlistview/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.customlistview;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    int flags[] = {R.drawable.me, R.drawable.person1, R.drawable.person2, R.drawable.person3, R.drawable.person4, R.drawable.me};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
+        simpleList.setAdapter(customAdapter);
     }
 }
