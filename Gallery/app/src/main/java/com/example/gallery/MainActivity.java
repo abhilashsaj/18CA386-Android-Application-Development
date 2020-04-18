8e0cd37c529a5063c3dd208e45444d8048dd78d0
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-18 20:03:55 +0530

    Added Images

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.java
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GridView gv = (GridView) findViewById(R.id.gvDetails);
+        GridView gv = (GridView) findViewById(R.id.gridview);
         gv.setAdapter(new ImageAdapter(this));
         gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(MainActivity.this, "Image Position: " + position, Toast.LENGTH_SHORT).show();
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
             }
         });
     }
