04da21e74d974bc69d9208919f5a8f7309b8e690
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-19 13:28:24 +0530

    rewriting java

--- SongList/app/src/main/java/com/example/songlist/MainActivity.java
+++ SongList/app/src/main/java/com/example/songlist/MainActivity.java
@@ -1,4 +1,29 @@
 package com.example.songlist;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView gv = (ListView) findViewById(R.id.list);
+        gv.setAdapter(new CustomAdapter(this));
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), SongDetails.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
 }
