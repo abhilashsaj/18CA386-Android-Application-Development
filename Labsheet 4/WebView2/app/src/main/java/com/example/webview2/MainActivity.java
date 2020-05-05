f2bbc7ad44d08a0ccea44e7be2cea4cb78c564ee
Abhilash Saj <abhilashsaj@gmail.com>
2020-05-05 12:20:14 +0530

    webview 2

--- Labsheet 4/WebView2/app/src/main/java/com/example/webview2/MainActivity.java
+++ Labsheet 4/WebView2/app/src/main/java/com/example/webview2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.webview2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView simpleWebView = (WebView) findViewById(R.id.simpleWebView);
+
+// set web view client
+        simpleWebView.setWebViewClient(new MyWebViewClient());
+
+// string url which you have to load into a web view
+        String url = "https://google.com/";
+        simpleWebView.getSettings().setJavaScriptEnabled(true);
+        simpleWebView.loadUrl(url);
     }
 }
