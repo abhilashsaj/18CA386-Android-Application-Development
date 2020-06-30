144eb2527ad63bd4edc17ddbd9992eff0cc7c3f3
Abhilash Saj <abhilashsaj@gmail.com>
2020-06-30 19:33:57 +0530

    database

--- Database/Practise1/app/src/main/java/com/example/practise1/MainActivity.java
+++ Database/Practise1/app/src/main/java/com/example/practise1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.practise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SQLiteDatabase mydatabase = openOrCreateDatabase("your database name",MODE_PRIVATE,null);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS TutorialsPoint(Username VARCHAR,Password VARCHAR);");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('admin','admin');");
+        Cursor resultSet = mydatabase.rawQuery("Select * from TutorialsPoint",null);
+        resultSet.moveToFirst();
+        String username = resultSet.getString(0);
+        String password = resultSet.getString(1);
     }
 }
