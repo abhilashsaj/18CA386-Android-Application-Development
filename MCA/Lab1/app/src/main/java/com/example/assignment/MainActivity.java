5bf0108251512a67b099282e78351b1c8246e9e9
Abhilash Saj <abhilashsaj@gmail.com>
2020-08-25 14:33:40 +0530

    ques5

--- MCA/Lab1/app/src/main/java/com/example/assignment/MainActivity.java
+++ MCA/Lab1/app/src/main/java/com/example/assignment/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +22,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         login = (Button) findViewById(R.id.login);
         uname =  (TextView) findViewById((R.id.editText)) ;
-        pass =  (TextView) findViewById((R.id.editText)) ;
+        pass =  (TextView) findViewById((R.id.editText2)) ;
         login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.login:
+                        alertDialog();
+                        break;
+                }
             Toast.makeText(getApplicationContext(), "Hey" + uname.getText() +" " +pass.getText(), Toast.LENGTH_LONG).show();//display the text of button1
             }
         });
+    }
+    private void alertDialog() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage(uname.getText() +" " +pass.getText());
+        dialog.setTitle("Dialog Box");
+        dialog.setPositiveButton("YES",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Toast.makeText(getApplicationContext(),"Yes is clicked",Toast.LENGTH_LONG).show();
+                    }
+                });
+        dialog.setNegativeButton("cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"cancel is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
     }
 }
