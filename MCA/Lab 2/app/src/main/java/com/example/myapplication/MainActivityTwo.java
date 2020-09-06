7240b6844225ab1408ec87ec8f18d33888e8a933
Abhilash Saj <abhilashsaj@gmail.com>
2020-09-06 19:21:29 +0530

    ques1

--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityTwo.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityTwo.java
@@ -1,4 +1,27 @@
 package com.example.myapplication;
 
-public class MainActivityTwo {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActtivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitytwo_main);
+        Bundle extras = getIntent().getExtras();
+        String value1 = extras.getString("Value1");
+        String value2 = extras.getString("Value2");
+        Toast.makeText(getApplicationContext(),"Values are:\n First value: "+value1+
+                "\n Second Value: "+value2, Toast.LENGTH_LONG).show();
+    }
+    public void callFirstActivity(View view){
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
 }
