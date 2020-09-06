3edbb0718a5f6fa417885284f91afefa4902c9b0
Abhilash Saj <abhilashsaj@gmail.com>
2020-09-06 19:44:59 +0530

    ques1

--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("Main Activity","Created Main Activity");
         setContentView(R.layout.activity_main);
     }
     public void callSecondActivity(View view){
+        Log.e("Calling Second Activity","Calling Activity two ");
         Intent i = new Intent(getApplicationContext(), MainActivityTwo.class);
         i.putExtra("Value1", "Android By Javatpoint");
         i.putExtra("Value2", "Simple Tutorial");
--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityTwo.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityTwo.java
@@ -2,17 +2,19 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActtivityTwo extends AppCompatActivity {
+public class MainActivityTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitytwo_main);
+        Log.e("Main Activity Two","Created Main Activity Two");
         Bundle extras = getIntent().getExtras();
         String value1 = extras.getString("Value1");
         String value2 = extras.getString("Value2");
@@ -20,6 +22,7 @@ public class MainActtivityTwo extends AppCompatActivity {
                 "\n Second Value: "+value2, Toast.LENGTH_LONG).show();
     }
     public void callFirstActivity(View view){
+        Log.e("Calling First Activity","Calling Main Activity ");
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
     }
