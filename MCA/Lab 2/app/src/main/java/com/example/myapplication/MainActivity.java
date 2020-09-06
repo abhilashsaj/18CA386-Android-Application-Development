0cb9eca635cef3326682d8f60163522ada3d8f4b
Abhilash Saj <abhilashsaj@gmail.com>
2020-09-06 20:59:29 +0530

    ques1

--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,22 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String STATE_USER = "Abhilash";
+    private String mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e("Main Activity","Created Main Activity");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mUser = savedInstanceState.getString(STATE_USER);
+        } else {
+            mUser = "user";
+        }
+        Toast.makeText(getApplicationContext(),"User info: "+ mUser, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString(STATE_USER, mUser);
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
     }
     public void callSecondActivity(View view){
         Log.e("Calling Second Activity","Calling Activity two ");
         Intent i = new Intent(getApplicationContext(), MainActivityTwo.class);
         i.putExtra("Value1", "Hi");
         i.putExtra("Value2", "Man");
-        // Set the request code to any code you like, you can identify the
-        // callback via this code
         startActivity(i);
     }
     public void callThirdActivity(View view){
@@ -29,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(getApplicationContext(), MainActivityThree.class);
         i.putExtra("Value1", "Hi");
         i.putExtra("Value2", "Man");
-        // Set the request code to any code you like, you can identify the
-        // callback via this code
         startActivity(i);
     }
     protected void onStart() {
--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityThree.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivityThree.java
@@ -1,4 +1,54 @@
 package com.example.myapplication;
 
-public class MainActivityThree {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitythree_main);
+
+        Log.e("Main Activity Three","Created Main Activity Three");
+        Bundle extras = getIntent().getExtras();
+        String value1 = extras.getString("Value1");
+        String value2 = extras.getString("Value2");
+        Toast.makeText(getApplicationContext(),"Values are:\n First value: "+value1+
+                "\n Second Value: "+value2, Toast.LENGTH_LONG).show();
+    }
+    public void callFirstActivity(View view){
+        Log.e("Calling First Activity","Calling Main Activity ");
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+    protected void onStart() {
+        super.onStart();
+        Log.e("Main Activity Three", "Main Activity Three Started, onstart invoked");
+    }
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("Main Activity Three", "Main Activity Three Started, onstart invoked");
+    }
+    protected void onResume() {
+        super.onResume();
+        Log.e("Main Activity Three", "Main Activity Three Resumed, onResume invoked");
+    }
+    protected void onPause() {
+        super.onPause();
+        Log.e("Main Activity Three", "Main Activity Three Pause, onPause invoked");
+    }
+    protected void onStop() {
+        super.onStop();
+        Log.e("Main Activity Three", "Main Activity Three stopped, onStop invoked");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Main Activity Three", "Main Activity Three Destroyed, onDestroy invoked");
+    }
 }
