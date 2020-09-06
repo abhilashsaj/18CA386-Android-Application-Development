b08861e8622a8325b52dc814eea50f6272543466
Abhilash Saj <abhilashsaj@gmail.com>
2020-09-06 18:37:18 +0530

    ques1

--- MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MCA/Lab 2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
-package com.example.myapplication;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
     }
-}
+    public void callSecondActivity(View view){
+        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+        i.putExtra("Value1", "Android By Javatpoint");
+        i.putExtra("Value2", "Simple Tutorial");
+        // Set the request code to any code you like, you can identify the  
+        // callback via this code  
+        startActivity(i);
+    }
+
+}  
