3d98d0d9969c1d1cb784e72b611949ca42a59289
Abhilash Saj <abhilashsaj@gmail.com>
2020-09-07 14:24:48 +0530

    ques4

--- MCA/Lab 2/Ques4/app/src/main/java/com/example/ques4/MainActivity.java
+++ MCA/Lab 2/Ques4/app/src/main/java/com/example/ques4/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.ques4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submit;
+    EditText name, password, email, contact, date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = (EditText) findViewById(R.id.editText1);
+        password = (EditText) findViewById(R.id.editText2);
+        email = (EditText) findViewById(R.id.editText3);
+        date = (EditText) findViewById(R.id.editText4);
+        contact = (EditText) findViewById(R.id.editText5);
+        submit = (Button) findViewById(R.id.button);
 
-    }
-
-    public void callSecondActivity(View view){
-        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
-        i.putExtra("Value1", "Android By Javatpoint");
-        i.putExtra("Value2", "Simple Tutorial");
-        // Set the request code to any code you like, you can identify the
-        // callback via this code
-        startActivity(i);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+                intent.putExtra("name", name.getText().toString());
+                intent.putExtra("password", password.getText().toString().isEmpty());
+                intent.putExtra("email", email.getText().toString().isEmpty());
+                intent.putExtra("date", date.getText().toString());
+                intent.putExtra("contact", contact.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
